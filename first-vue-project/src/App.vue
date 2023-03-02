98efd0727aaafd612e25cdd67de1dcf53d9e6ad8
EZreal-Map <tangkaiwen00@gmail.com>
2023-03-03 00:27:46 +0800

    配置首页大屏与一张图的路由

--- first-vue-project/src/App.vue
+++ first-vue-project/src/App.vue
@@ -5,7 +5,9 @@
       <el-container class="app-content-pannel">
         <el-aside class="sys-menu">
           <el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
-            background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" :collapse="true">
+            @select="handleMenuSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b"
+            :collapse="true">
+            <!-- @ === v-on -->
             <el-menu-item index="1">
               <i class="el-icon-monitor"></i>
               <span slot="title">首页大屏</span>
@@ -17,7 +19,8 @@
           </el-menu>
         </el-aside>
         <el-main class="sys-content">
-          <MapView />
+          <router-view></router-view>
+          <!-- <MapView /> -->
         </el-main>
       </el-container>
     </el-container>
@@ -25,22 +28,25 @@
 </template>
 
 <script>
-import MapView from './components/common/MapView.vue';
+// import MapView from './components/common/MapView.vue';
 
 export default {
   name: 'App',
   components: {
-    MapView,
+    // MapView,
   },
   methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    handleMenuSelect(index) {
+      if (index === '1') {
+        // 跳转到首页大屏
+        this.$router.push('/')
+      } else if (index === '2') {
+        // 跳转一张图
+        this.$router.push('/onemap')
+      }
     },
   },
-}
+};
 </script>
 
 <style>
